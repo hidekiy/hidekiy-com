7d47808926736b47b9498b265635870fbd2f6a7d
Hideki Yamamura <hideki.yamamura@gmail.com>
2018-05-18 04:37:43 +0900

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+const gulp = require('gulp');
+const handlebars = require('gulp-compile-handlebars');
+const rename = require('gulp-rename');
+
+gulp.task('handlebars', () => {
+    return gulp.src('src/pages/**/*.hbs')
+        .pipe(handlebars({}, {
+        	ignorePartials: true,
+        	batch : ['src/partials'],
+        }))
+        .pipe(rename({extname: '.html'}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('static', () => {
+    return gulp.src('src/static/**/*')
+    	.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['handlebars', 'static']);
