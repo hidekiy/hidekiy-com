4e5d85cd086c2f3f4c4445b6e28542380c90969e
Hideki Yamamura <hideki.yamamura@gmail.com>
2020-09-05 16:53:54 +0900

    add sitemap

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,45 @@
 const gulp = require('gulp');
 const del = require('del');
 const hb = require('gulp-hb');
+const sitemap = require('gulp-sitemap');
+
+const siteUrl = 'https://hidekiy.com';
+const dist = 'dist';
 
 gulp.task('clean', () => {
-    return del(['dist']);
+    return del(dist);
 });
 
-gulp.task('handlebars', gulp.series('clean', () => {
+gulp.task('handlebars', () => {
     return gulp.src('src/templates/**/*.html')
         .pipe(hb().partials('src/partials/**/*.hbs'))
-        .pipe(gulp.dest('dist'));
-}));
+        .pipe(gulp.dest(dist));
+});
 
-gulp.task('static', gulp.series('clean', () => {
+gulp.task('static', () => {
     return gulp.src('src/static/**/*')
-        .pipe(gulp.dest('dist'));
-}));
+        .pipe(gulp.dest(dist));
+});
 
-gulp.task('build', gulp.parallel('handlebars', 'static'));
+gulp.task('sitemap', () => {
+    return gulp.src('dist/**/*.html', {
+            read: false
+        })
+        .pipe(sitemap({
+            siteUrl,
+            mappings: [
+                {
+                    pages: ['**/*'],
+                    getLoc(siteUrl, loc, entry) {
+                        // Removes the file extension if it exists
+                        return loc.replace(/\.\w+$/, '');
+                    }
+                }
+            ]
+        }))
+        .pipe(gulp.dest(dist));
+});
 
-gulp.task('default', gulp.task('build'));
+gulp.task('build', gulp.series(gulp.parallel('handlebars', 'static'), 'sitemap'));
+
+gulp.task('default', gulp.series('clean', 'build'));
